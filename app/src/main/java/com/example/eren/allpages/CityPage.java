d7781192095cedb74a433953093d13d428a40fbc
hkmtsm <hkmtsm@gmail.com>
2017-02-28 14:10:41 +0300

    City page (Nihan, Hikmet)

---
+++ app/src/main/java/com/example/eren/allpages/CityPage.java
@@ -0,0 +1,13 @@
+package com.example.eren.allpages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CityPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_page);
+    }
+}
